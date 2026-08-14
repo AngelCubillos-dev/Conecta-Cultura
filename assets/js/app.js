9cd3c5704ce266238a26302dba0595db9e6e0906
AV-Alumno <AV-Alumno@duoc.acad>
2026-08-14 12:42:05 -0400

    feat: construir estructura semántica de inicio y nosotros y agregar noticias y primer detalle

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,7 @@
 const botonBienvenida = document.querySelector("#boton-bienvenida");
 
-botonBienvenida.addEventListener("click", () => {
-  alert("Primera versión de Conecta Cultura.");
-});
+if (botonBienvenida) {
+  botonBienvenida.addEventListener("click", () => {
+    alert("Conecta Cultura se construye paso a paso.");
+  });
+}
